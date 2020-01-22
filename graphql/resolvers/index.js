c6eecd228edeabb359506799bee326cd2e2019c0
kimlie0401 <54561489+kimlie0401@users.noreply.github.com>
2020-01-21 17:23:26 -0800

    #7 Dynamic Relations Part2

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -3,21 +3,37 @@ const bcrypt = require("bcryptjs");
 const Event = require("../../models/event");
 const User = require("../../models/user");
 
-const events = eventIds => {
-  return Event.find({ _id: { $in: eventIds } })
-    .then(events => {
-      return events.map(event => {
-        return {
-          ...event._doc,
-          _id: event.id,
-          date: new Date(event._doc.date).toISOString(),
-          creator: user.bind(this, event.creator)
-        };
-      });
-    })
-    .catch(err => {
-      throw err;
+// const events = eventIds => {
+//   return Event.find({ _id: { $in: eventIds } })
+//     .then(events => {
+//       return events.map(event => {
+//         return {
+//           ...event._doc,
+//           _id: event.id,
+//           date: new Date(event._doc.date).toISOString(),
+//           creator: user.bind(this, event.creator)
+//         };
+//       });
+//     })
+//     .catch(err => {
+//       throw err;
+//     });
+// };
+
+const events = async eventIds => {
+  try {
+    const events = await Event.find({ _id: { $in: eventIds } });
+    return events.map(event => {
+      return {
+        ...event._doc,
+        _id: event.id,
+        date: new Date(event._doc.date).toISOString(),
+        creator: user.bind(this, event.creator)
+      };
     });
+  } catch (err) {
+    throw err;
+  }
 };
 
 const user = async userId => {
