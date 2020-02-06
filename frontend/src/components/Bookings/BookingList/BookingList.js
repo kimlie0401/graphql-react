6ecad884f535659bb31279afe6ead428465e4861
kimlie0401 <54561489+kimlie0401@users.noreply.github.com>
2020-02-06 13:20:16 -0800

    #19 Cancel Bookings

---
+++ frontend/src/components/Bookings/BookingList/BookingList.js
@@ -0,0 +1,51 @@
+import React from "react";
+import styled from "styled-components";
+
+const List = styled.ul`
+  width: 40rem;
+  max-width: 90%;
+  margin: 2rem auto;
+  list-style: none;
+  padding: 0;
+`;
+
+const Button = styled.button`
+  background-color: rgba(46, 44, 44, 0.8);
+  font: inherit;
+  border: 1px solid rgba(46, 44, 44, 0.8);
+  border-radius: 3px;
+  padding: 0.25rem 1rem;
+  margin-right: 1rem;
+  box-shadow: 0px 1px 5px 2px rgba(0, 0, 0, 0.8);
+  color: white;
+  cursor: pointer;
+`;
+
+const Item = styled.li`
+  margin: 1rem 0;
+  padding: 1rem;
+  border: 1px solid rgba(46, 44, 44, 0.8);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const bookingList = props => (
+  <List>
+    {props.bookings.map(booking => {
+      return (
+        <Item key={booking._id}>
+          <div>
+            {booking.event.title} -{" "}
+            {new Date(booking.createdAt).toLocaleDateString()}
+          </div>
+          <div>
+            <Button onClick={() => props.onDelete(booking._id)}>Cancel</Button>
+          </div>
+        </Item>
+      );
+    })}
+  </List>
+);
+
+export default bookingList;
--- frontend/src/pages/Bookings.js
+++ frontend/src/pages/Bookings.js
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from "react";
 import AuthContext from "../context/auth-context";
 import Loader from "../components/Loader/Loader";
+import BookingList from "../components/Bookings/BookingList/BookingList";
 
 class BookingsPage extends Component {
   state = {
@@ -57,22 +58,61 @@ class BookingsPage extends Component {
       });
   };
 
+  deleteBookingHandler = bookingId => {
+    let confirmToDelete = window.confirm("Are you sure to cancel?!");
+    if (confirmToDelete) {
+      const requestBody = {
+        query: `
+          mutation {
+            cancelBooking(bookingId:"${bookingId}") {
+              _id
+              title
+              }
+            }
+        `
+      };
+
+      const token = this.context.token;
+
+      fetch("http://dkim0401.mooo.com:4588/graphql", {
+        method: "POST",
+        body: JSON.stringify(requestBody),
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + token
+        }
+      })
+        .then(res => {
+          if (res.status !== 200 && res.status !== 201) {
+            throw new Error("Failed!");
+          }
+          return res.json();
+        })
+        .then(resData => {
+          this.setState(prevState => {
+            const updatedBookings = prevState.bookings.filter(booking => {
+              return booking._id !== bookingId;
+            });
+            return { bookings: updatedBookings, loading: false };
+          });
+        })
+        .catch(err => {
+          console.log(err);
+          this.setState({ loading: false });
+        });
+    }
+  };
+
   render() {
     return (
       <Fragment>
         {this.state.loading ? (
           <Loader />
         ) : (
-          <Fragment>
-            <ul>
-              {this.state.bookings.map(booking => (
-                <li key={booking._id}>
-                  {booking.event.title} -{" "}
-                  {new Date(booking.createdAt).toLocaleDateString()}
-                </li>
-              ))}
-            </ul>
-          </Fragment>
+          <BookingList
+            bookings={this.state.bookings}
+            onDelete={this.deleteBookingHandler}
+          />
         )}
       </Fragment>
     );
